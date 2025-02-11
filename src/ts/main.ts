7dd0cca324341e559e3b00e075366bed97905d50
Qori El-Hafizh <qori@qori.dev>
2025-02-11 21:27:25 +0700

    ApplicationState

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,4 @@
-import van from "vanjs-core";
-import {State} from "vanjs-core";
+import van, {State} from "vanjs-core";
 
 type Data = {
     quizzes: Quiz[]
@@ -17,16 +16,44 @@ type Question = {
     answer: string
 }
 
-type QuizRun = {
-    quiz: Quiz,
-    currentQuestionIndex: number,
-    answers: string[],
-    areCorrect: boolean[],
-    finished: boolean
+class ApplicationState {
+    quiz: State<Quiz|null>
+    currentQuestionIndex: State<number>
+    answers: State<string[]>
+    currentAnswerIsCorrect: State<boolean>
+    finished: State<boolean>
+
+    constructor(){
+        this.quiz = van.state(null);
+        this.answers = van.state([]);
+        this.currentQuestionIndex = van.state(0);
+        this.currentAnswerIsCorrect = van.derive(()=>{
+            const currentAnswer = this.answers.val[this.currentQuestionIndex.val];
+            const correctAnswer = this.quiz.val?.questions[this.currentQuestionIndex.val].answer;
+            if (correctAnswer === undefined) {
+                return false;
+            }
+
+            if (currentAnswer === correctAnswer) {
+                return true;
+            }
+
+            return false
+        });
+
+        this.finished = van.derive(()=>this.answers.val.length === this.quiz.val?.questions.length);
+    }
+
+    reset(){
+        this.quiz.val = null;
+        this.currentQuestionIndex.val = 0;
+        this.answers.val = [];
+    }
 }
 
 (async ()=>{
     const response = await fetch("/data.json");
     const data: Data = await response.json();
+    const appState = new ApplicationState();
 })()
 
