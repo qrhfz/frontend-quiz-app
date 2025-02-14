fe10230e2557f25bc8f6b5d5c54169e6c6b9482e
Qori El-Hafizh <qori@qori.dev>
2025-02-14 17:19:01 +0700

    switch button

--- src/ts/App.ts
+++ src/ts/App.ts
@@ -1,9 +1,25 @@
 import van from "vanjs-core";
 
-const {div, button} = van.tags
+const {div, button, input} = van.tags
 
 export default function App() {
     return div(
-        button({class:"btn-primary"}, "Button Idle")
+        button({class:"btn-primary"}, "Button Idle"),
+        Switch()
+    )
+}
+
+function Switch(props?:any){
+    const checkbox = input({type:"checkbox",...props});
+
+    return div({class:"switch"},
+        checkbox,
+        div({
+            class:"switch-look",
+            onclick: ()=>{
+                checkbox.click();
+            }},
+            div({class:"switch-circle"})
+        )
     )
 }
