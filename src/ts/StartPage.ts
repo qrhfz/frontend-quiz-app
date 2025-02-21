98ff16cd6e02163aaa39fe8fda8d5bf737f59ba5
Qori El-Hafizh <qori@qori.dev>
2025-02-21 18:55:53 +0700

    start page layouting

--- src/ts/StartPage.ts
+++ src/ts/StartPage.ts
@@ -1,6 +1,6 @@
 import van from "vanjs-core";
-
-const { div, h1, strong, p, ul, li, span, br } = van.tags;
+import { Switch } from "./Switch"
+const { main, header, div, h1, strong, p, ul, li, span, br } = van.tags;
 
 export function StartPage() {
     const quizTitles = [
@@ -10,14 +10,23 @@ export function StartPage() {
         "Accessibility"
     ]
 
-    return div(
-        h1({ class: "txt-hl" },
-            "Welcome to the ", br(), strong({ class: "txt-hl-b" }, "Frontend Quiz!")
+    return div({ class: "container", style: "margin-top: 97px;" },
+        header(
+            { style: "display: flex; justify-content: end; margin-bottom: 99px;" },
+            Switch()
         ),
-        p("Pick a subject to get started."),
-        ul({ style: "padding:0; display: flex: flex-direction: column; gap: 24px;" },
-            quizTitles.map((t) =>
-                li({ class: "list-tile" }, span({ class: "txt-hs" }, t)))
+        main(
+            { style: "display: grid; grid-template-columns: repeat(2, 1fr);" },
+            div(
+                h1({ class: "txt-hl" },
+                    "Welcome to the ", br(), strong({ class: "txt-hl-b" }, "Frontend Quiz!")
+                ),
+                p("Pick a subject to get started."),
+            ),
+            ul({ class: "vertical-menu" },
+                quizTitles.map((t) =>
+                    li({ class: "menu-item" }, span({ class: "txt-hs" }, t)))
+            )
         )
     )
 }
